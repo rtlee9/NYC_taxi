6b4eab8ff1f251494fb53ec703e27b365c630bee
Ryan Lee <ryantlee9@gmail.com>
2016-08-01 08:24:06 -0400

    Large query for batch distance api calls

--- 2.Code/3.AnalysisRoundaboutRides/RandomSampleDistances.R
+++ 2.Code/3.AnalysisRoundaboutRides/RandomSampleDistances.R
@@ -12,7 +12,8 @@ library(ggthemes, quietly = T)
 library(rCharts, quietly = T)
 library(knitr, quietly = T)
 library(vegan, quietly = T)
-setwd("/Users/Ryan/Github/NYC_taxi/2.Code/2.Analysis")
+library(pastecs)
+setwd("/Users/Ryan/Github/NYC_taxi/2.Code/3.AnalysisRoundaboutRides/")
 
 # Load maps
 map_center <- c(lon = -73.94, lat = 40.75)
@@ -24,10 +25,16 @@ NYC_map_bw <- ggmap(NYC_bw, extent = "device")
 # Query data from PSQL server
 pg = dbDriver("PostgreSQL")
 con = dbConnect(pg, password="", host="localhost", port=5432)
-fileName <- 'query_rand2500.sql'
-query_rand2500 <- readChar(fileName, file.info(fileName)$size)
-rand_trips <- as.data.table(dbGetQuery(con, query_rand2500))
+fileName <- 'query_rand100000.sql'
+query_rand100000 <- readChar(fileName, file.info(fileName)$size)
+rand_trips <- as.data.table(dbGetQuery(con, query_rand100000))
 hold <- dbDisconnect(con)
+
+# Save full random sample (one time only)
+saveRDS(rand_trips, "../../3.Analysis/rand_trips_full.Rda")
+
+# Set run number
+
 
 # Get expected distances for random sample
 rand_trips_sample <- rand_trips[1:1000] # TEMP
@@ -36,6 +43,12 @@ rand_trips_sample$to <- paste(rand_trips_sample$dropoff_latitude, rand_trips_sam
 gdist <- as.data.table(mapdist(from = from, to = to, mode = 'driving', output = 'simple', messaging = F))
 distQueryCheck()
 
-# Merge back with trip data
+# Merge back with trip data and save
 rand_trips_mapped <- cbind(rand_trips_sample, gdist)
+saveRDS(rand_trips_mapped, paste0("rand_trips_mapped_", today(), ".Rda"))
+
+# Distribution of actual v expected distance
 rand_trips_mapped[, actualExepcted := trip_distance/miles]
+ggplot(rand_trips_mapped[actualExepcted < 3], aes(x=actualExepcted)) + geom_density()
+quantile(rand_trips_mapped[!is.na(actualExepcted)]$actualExepcted, c(.01, .05, .5, .95, .99))
+
---
+++ 2.Code/3.AnalysisRoundaboutRides/query_rand100000.sql
@@ -0,0 +1,12 @@
+select t.*
+from taxi_nhood_full t
+inner join zillow_sp p
+  on t.pick_zgid = p.gid
+inner join zillow_sp d
+  on t.drop_zgid = d.gid
+where 1=1
+  and p.city = 'New York City-Manhattan'
+  and d.city = 'New York City-Manhattan'
+order by random()
+limit 100000
+;
