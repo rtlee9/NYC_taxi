5ee8d8a5ca2b78d4a0056514837608c458c05fbf
Ryan Lee <ryantlee9@gmail.com>
2016-09-06 22:52:46 -0400

    Exclude null coordinates from random sample

--- 2.Code/3.AnalysisRoundaboutRides/1.Create_random_sample.R
+++ 2.Code/3.AnalysisRoundaboutRides/1.Create_random_sample.R
@@ -17,7 +17,10 @@ if (any(!unlist(loadSuccess))) stop(paste("\n\tPackage load failed:", reqPackage
 data_path <- "1.Data/"
 analysis_path <- "3.Analysis/"
 
-# Taxi data
+# Read data from local machine, and exclude null coordinates
 taxi_14 <- fread(paste0(data_path, "nyc_taxi_data.csv"))
+taxi_14 <- taxi_14[pickup_longitude != 0 & pickup_latitude != 0 & dropoff_longitude != 0 & dropoff_latitude != 0]
+
+# Save random sample of sample size sample_size
 sample <- taxi_14[sample(.N, sample_size)]
 saveRDS(sample, paste0(analysis_path, "taxi_14_sample_", sample_size, ".Rda"))
